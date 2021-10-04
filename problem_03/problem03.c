d527ca9c6eaf780351e4fde8e82bbe6600cac4a5
Rafael <rafael.rbranco@hotmail.com>
2021-10-04 00:30:01 -0300

    Problem 3 Solved

--- problem_03/problem03.c
+++ problem_03/problem03.c
@@ -1,37 +1,36 @@
 #include <stdio.h>
+#include <math.h>
 
-int isPrime(long long int n){
-	long long int i;
-	long long int count = 0;
-	
-	for(i = 2; i <= n / 2; ++i){
-		if(n % i == 0){ 
-			count = 1;
-		}
-	}
-	
-	if(count == 1){
-		return 0;
-	}else{
-		return 1;
-	}
-}
-
+int isPrime(long long int);
 
 int main(){
 	
 	long long int n = 600851475143;
 	long long int i;
-	//n = 13195;
 	
-	for(i = n - 1; i >= 1; i--){
-		if(isPrime(i) && n % i == 0){
-			printf("%d ", i);
-			break;
+	for(i = sqrt(n - 1) ; i > 1; --i){
+		if(n % i == 0){
+			if(isPrime(i)){
+				printf("%d ", i);
+				break;
+			}	
 		}
 	}
 	
-		
-	
 	return 0;
+}
+
+
+int isPrime(long long int n){
+	
+	long long int i;
+	int flag = 0;
+	
+	for(i = 2; i <= sqrt(n); ++i){
+		if(n % i == 0){
+			return 0;
+		}
+	}
+	
+	return 1;
 }
